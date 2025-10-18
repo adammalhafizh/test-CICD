ebf6a34467395ef72c9c57417981e4b8433ac8a9
adam <adamalhafizh23@gmail.com>
2025-10-18 12:44:13 +0700

    workflow test

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ export default function App() {
         <Route path="/lifegoals" element={<LifeGoals />} />
         <Route path="/lifegoal/:id" element={<LifeGoalDetail />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/otpLogin" element={<OtpLogin />} />
-        <Route path="/otpLogin" element={<OtpLogin />} />
-        <Route path="/otpLogin" element={<OtpLogin />} />
+        <Route path="/popupblock" element={<PopupBlock />} />
+        <Route path="/popupblock" element={<PopupBlock />} />
+        <Route path="/popupblock" element={<PopupBlock />} />
         <Route path="/popupblock" element={<PopupBlock />} />
       </Routes>
     </AnimatePresence>
