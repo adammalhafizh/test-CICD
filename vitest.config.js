8f0210a53e4f5e3be0dad51d855b5851491acdd0
adam <adamalhafizh23@gmail.com>
2025-10-18 17:13:52 +0700

    fixing bugs

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
   test: {
-    environment: "jsdom",
-    globals: true,
+    environment: "jsdom", // agar bisa test komponen React
+    include: ["src/**/*.{test,spec}.{js,jsx,ts,tsx}"], // cari file test di folder src
   },
 });
